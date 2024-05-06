9297d0a70d0b2a03fde82b232440b90612810981
Jefferson santos <jefferson@Dev.Jefferson>
2024-05-06 21:37:53 +0100

    comimit de atualizacao

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -6,19 +6,19 @@ import { Link } from 'react-router-dom';
 
 export default function Menu() {
     return (
-        <section className='menu'>
-            <a className='social' href="www.com">
+        <div className='menu'>
+            <a className='social' href="https://www.instagram.com/jeffin.souza21/" target='blanck'>
                 <BsInstagram color='#fff' size={24
 
                 } />
             </a>
-            <a className='social' href="www.com">
-                <FaLinkedin color='#fff' size={24} />
+            <a className='social' href="https://www.linkedin.com/in/jefferson-santos-33645521a/" target='blank'>
+                <FaLinkedin color='#fff' size={27} />
             </a>
 
             <Link className='menu-item' to='links'>
                 Meus Links
             </Link>
-        </section>
+        </div>
     )
 }
