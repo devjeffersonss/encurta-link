3e26f53704fc378eb9caa76c46d676cf64aab2c4
Jefferson santos <jefferson@Dev.Jefferson>
2024-05-12 15:46:10 +0100

    Atualização

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import './footer.css'
 export default function Footer() {
     return (
         <footer className='rodape'>
-            <p>&copy; Todos os direitos reservado!</p>
+            <p>&copy; Todos os direitos reservados!</p>
         </footer>
 
     )
